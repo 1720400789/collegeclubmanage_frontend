43e18de3c7ebee286f56db0604a079573b807fbc
zjluvcn <hello@zj.cn>
2023-05-30 18:43:58 +0800

    高校社团管理系统前端项目代码初始搭建

---
+++ src/utils/http.js
@@ -0,0 +1,21 @@
+//axios基础的封装，如：接口基地址、接口超时时间、请求拦截器和响应拦截器
+import axios from "axios"
+
+//1.接口基地址和接口超时时间的封装
+const httpInstance = axios.create({
+    baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+    timeout: 5000
+})
+
+// 2.axios请求拦截器
+httpInstance.interceptors.request.use(config => {
+    return config
+}, e => Promise.reject(e))
+
+// 3.axios响应式拦截器
+httpInstance.interceptors.response.use(res => res.data, e => {
+    return Promise.reject(e)
+})
+
+//导出配置
+export default httpInstance
